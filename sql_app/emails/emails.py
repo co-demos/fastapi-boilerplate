380798d94b4abc9637087c4e9c1b4ae249850f69
Julien_P <jparis.py@gmail.com>
2021-04-07 13:37:04 +0200

    emailing

--- sql_app/emails/emails.py
+++ sql_app/emails/emails.py
@@ -10,7 +10,23 @@ from ..core.config import settings
 import pprint
 pp = pprint.PrettyPrinter(indent=1)
 
+
 cwd = Path.cwd()
+server_host = settings.SERVER_HOST
+api_version = settings.API_V1_STR
+
+smtp_options = {
+  "host": settings.SMTP_HOST,
+  "port": settings.SMTP_PORT
+}
+if settings.SMTP_TLS:
+  smtp_options["tls"] = True
+if settings.SMTP_USER:
+  smtp_options["user"] = settings.SMTP_USER
+if settings.SMTP_PASSWORD:
+  smtp_options["password"] = settings.SMTP_PASSWORD
+
+
 
 def send_email(
   email_to: str,
@@ -19,13 +35,16 @@ def send_email(
   environment: Dict[str, Any] = {},
   ):
 
-  print(">>>  send_email > settings : ...")
-  pp.pprint(settings.dict())
+  # print(">>>  send_email > settings : ...")
+  # pp.pprint(settings.dict())
 
   assert settings.EMAILS_ENABLED, "no provided configuration for email variables"
+  
+  with open(Path(f'{cwd}{settings.EMAIL_TEMPLATES_DIR}') / html_template ) as f:
+    template_str = f.read()
 
   subject = JinjaTemplate(subject_template)
-  template = JinjaTemplate(html_template)
+  template = JinjaTemplate(template_str)
   print(">>>  send_email > template : ...",  template)
 
   message = emails.Message(
@@ -33,16 +52,6 @@ def send_email(
     html=template,
     mail_from=(settings.EMAILS_FROM_NAME, settings.EMAILS_FROM_EMAIL),
   )
-  smtp_options = {
-    "host": settings.SMTP_HOST,
-    "port": settings.SMTP_PORT
-  }
-  if settings.SMTP_TLS:
-    smtp_options["tls"] = True
-  if settings.SMTP_USER:
-    smtp_options["user"] = settings.SMTP_USER
-  if settings.SMTP_PASSWORD:
-    smtp_options["password"] = settings.SMTP_PASSWORD
   
   response = message.send(
     to=email_to,
@@ -57,13 +66,15 @@ def send_test_email(
   ):
   project_name = settings.PROJECT_NAME
   subject = f"{project_name} - Test email"
-  with open(Path(f'{cwd}{settings.EMAIL_TEMPLATES_DIR}') / "test_email.html") as f:
-    template_str = f.read()
+  html_template = "test_email.html"
   send_email(
     email_to=email_to,
     subject_template=subject,
-    html_template=template_str,
-    environment={"project_name": settings.PROJECT_NAME, "email": email_to},
+    html_template=html_template,
+    environment={
+      "project_name": settings.PROJECT_NAME,
+      "email": email_to
+    },
   )
 
 
@@ -75,14 +86,12 @@ def send_reset_password_email(
   project_name = settings.APP_TITLE
   subject = f"{project_name} - Password recovery for user {email}"
   print(">>> send_reset_password_email > cwd : ", cwd )
-  with open(Path(f'{cwd}{settings.EMAIL_TEMPLATES_DIR}') / "reset_password.html") as f:
-    template_str = f.read()
-  server_host = settings.SERVER_HOST
-  link = f"{server_host}/reset-password?token={token}"
+  html_template = "reset_password.html"
+  link = f"{server_host}/{api_version}/users/reset-password?token={token}"
   send_email(
     email_to=email_to,
     subject_template=subject,
-    html_template=template_str,
+    html_template=html_template,
     environment={
       "project_name": project_name,
       "username": email,
@@ -100,13 +109,12 @@ def send_new_account_email(
   ):
   project_name = settings.APP_TITLE
   subject = f"{project_name} - New account for user {username}"
-  with open(Path('f{cwd}{settings.EMAIL_TEMPLATES_DIR}') / "new_account.html") as f:
-    template_str = f.read()
+  html_template = "new_account.html"
   link = settings.SERVER_HOST
   send_email(
     email_to=email_to,
     subject_template=subject,
-    html_template=template_str,
+    html_template=html_template,
     environment={
       "project_name": project_name,
       "username": username,
